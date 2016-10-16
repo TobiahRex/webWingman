dac46e2fefb9f0ac0715d78b55009418df5ed42f
TobiahRex <tobiah.rex@gmail.com>
2016-10-15 17:42:42 -0700

    created RegisterComponent - no actions attached to buttons yet.

--- src/Components/RegisterCard.js
+++ src/Components/RegisterCard.js
@@ -10,19 +10,21 @@ import {
 
 const muiCard = ({ title }) => (
   <Card>
-
     <CardText>
-      Props: { title || "'empty'" }
+      {title}
     </CardText>
 
     <CardText>
-      <TextField floatingLabelText="Email" />
+      <TextField floatingLabelText="Email" hintText="Email" />
       <br />
       <TextField floatingLabelText="Password" />
+      <br />
+      <TextField floatingLabelText="Confirm Password" />
+      <br />
     </CardText>
     <CardActions>
-      <FlatButton label="Forgot Password?" />
-      <RaisedButton label="Login" primary />
+      <FlatButton label="Need to Login?" />
+      <RaisedButton label="Register" secondary />
     </CardActions>
   </Card>
 );
