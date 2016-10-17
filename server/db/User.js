53036c55d735fe8a03e670c183314ccd7d3baf98
TobiahRex <tobiah.rex@gmail.com>
2016-10-16 22:37:02 -0700

    refactored User.js

--- server/db/User.js
+++ server/db/User.js
@@ -1,31 +1,31 @@
 /* global User*/
 import mongoose from 'mongoose';
 import Promise from 'bluebird';
-import jwt from 'json-web-token';
-import moment from 'moment';
+// import jwt from 'json-web-token';
+// import moment from 'moment';
 import bcryptJS from 'bcrypt';
 
 const bcrypt = Promise.promisify(bcryptJS);
-const JWT_SECRET = process.env.JWT_SECRET;
-const userSchema = new mongoose.schema({
+// const JWT_SECRET = process.env.JWT_SECRET;
+const userSchema = new mongoose.Schema({
 
 });
 
-userSchema.statics.obtainUsers = (cb) => {
-  User.find({}).exec()
+userSchema.statics.obtainUsers = function (cb) {
+  this.find({}).exec()
   .then(dbUsers => cb(null, dbUsers))
   .catch(err => cb(err));
 };
 
-userSchema.statics.registerNewUser = (newUser, cb) => {
-  User.findOne({ email: newUser.email }).exec()
+userSchema.statics.registerNewUser = function (newUser, cb) {
+  this.findOne({ email: newUser.email }).exec()
   .then((dbUser) => {
     if (dbUser) return cb({ ERROR: 'That user already exists' });
     return bcrypt.hashAsync(newUser.password, 12);
   })
   .then((hash) => {
     newUser.password = hash;
-    return User.create(newUser);
+    return this.create(newUser);
   })
   .then(dbUser => cb(null, dbUser))
   .catch(err => cb(err));
