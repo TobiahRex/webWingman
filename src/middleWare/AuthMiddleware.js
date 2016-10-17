0c8c18337ffed55d722287167376fb035c541d1c
TobiahRex <tobiah.rex@gmail.com>
2016-10-16 20:52:12 -0700

    refactored auth middleware.

--- src/middleWare/AuthMiddleware.js
+++ src/middleWare/AuthMiddleware.js
@@ -3,33 +3,30 @@ import * as types from "../actions/actionTypes";
 import * as UserActions from "../actions/UserActions";
 
 // This is middleware using THUNK.
-const CheckActiveUser = store =>
-next =>
-action => {
+const CheckActiveUser = store => next => action => {
   if (action.type !== types.CHECK_ACTIVE_USER) return next(action);
+
   if (localStorage.profile && localStorage.id_token) {
-    let profile = JSON.parse(localStorage.profile);
-    let headers = new Headers();
+    const profile = JSON.parse(localStorage.profile);
+    const headers = new Headers();
     headers.append("Content-Type", "application/json");
-    let options = {
+    const options = {
+      headers,
       method: "POST",
       credentials: "same-origin",
-      headers: headers,
       mode: "cors",
       cache: "default",
-      body: JSON.stringify({email: profile.email})
+      body: JSON.stringify({ email: profile.email }),
     };
     fetch("/api/users/login", options)
-    .then(response => {
-      return response.json();
-    })
-    .then(parsedResponse => {
+    .then(response => response.json())
+    .then((parsedResponse) => {
       profile.userBeerData = parsedResponse;
       store.dispatch(UserActions.activeUserConfirmed(profile));
       return next(action);
     })
-    .catch(error => {
-      console.log("Error: ", error);
+    .catch((error) => {
+      console.error('Error: ', error);
       return next(action);
     });
   } else {
