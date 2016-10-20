841960c57e2c48e8c53fbdd09fc9708324a5afa4
TobiahRex <tobiah.rex@gmail.com>
2016-10-19 22:29:49 -0700

    refactored RegisterContainer.js

--- src/Containers/RegisterContainer.js
+++ src/Containers/RegisterContainer.js
@@ -26,14 +26,14 @@ class RegisterContainer extends React.Component {
     this.errorProps = {
       title: 'Password Error',
       open: false,
-      onRequestClose: this.resetError,
+      onRequestClose: this.clearError,
       modal: true,
       actions: [
         <FlatButton
           primary
           label="OK"
-          onTouchTap={this.resetError}
-          />,
+          onTouchTap={this.clearError}
+        />,
       ],
     };
   }
@@ -59,25 +59,23 @@ class RegisterContainer extends React.Component {
   }
   submitNewUser = () => {
     if (this.state.confirmPassword !== this.state.password) {
-      this.setState({ error: 'Password do not match.  Please try again.' });
+      this.setState({ error: 'Passwords do not match.  Please try again.' });
       return (this.errorProps.open = true);
     }
     return this.props.registerUser(this.state);
   }
-  resetError = () => {
+  clearError = () => {
     this.setState({ error: '' });
     this.errorProps.open = false;
   }
-  render = () => {
-    console.log('this.errorProps: ', this.errorProps);
-    console.log('this.state: ', this.state);
-    return (
-      <div>
-        <RegisterCard {...this.RegisterProps} />
-        <Dialog {...this.errorProps} >{this.state.error}</Dialog>
-      </div>
-    );
-  };
+  render = () => (
+    <div>
+      <RegisterCard {...this.RegisterProps} />
+      <Dialog {...this.errorProps} >
+        {this.state.error}
+      </Dialog>
+    </div>
+  );
 }
 
 const mapDispatchToProps = dispatch => ({
