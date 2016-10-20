78f07a7680001d11948afea22bcef348cb6e09c9
TobiahRex <tobiah.rex@gmail.com>
2016-10-20 00:12:16 -0700

    added loginFailure to LoginSaga.js

--- src/Sagas/index.js
+++ src/Sagas/index.js
@@ -7,13 +7,14 @@ import CreateThing from './ThingSagas/CreateThing';
 import EditThing from './ThingSagas/EditThing';
 import RemoveThing from './ThingSagas/RemoveThing';
 import { ThingTypes } from '../Redux/ThingRedux';
+import { AuthTypes } from '../Redux/AuthRedux';
 
 // ----- Thing Sagas ----- //
 import RegisterUser from './AuthSaga/RegisterSaga';
 
 const api = API.createAPI();
 export default function* rootSaga() {
-  yield [takeLatest(ThingTypes.REGISTER_USER, RegisterUser, api),
+  yield [takeLatest(AuthTypes.REGISTER_USER, RegisterUser, api),
   takeLatest(ThingTypes.GET_ALL_THINGS, GetAllThings, api),
   takeLatest(ThingTypes.CREATE_THING, CreateThing, api),
   takeLatest(ThingTypes.REMOVE_THING, RemoveThing, api),
