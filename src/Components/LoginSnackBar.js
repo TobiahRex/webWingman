69c26c5ed007d6975fd2bbe2e09710c17f8228f9
TobiahRex <tobiah.rex@gmail.com>
2016-10-20 00:49:37 -0700

    modified LoginContainer.js

---
+++ src/Components/LoginSnackBar.js
@@ -0,0 +1,70 @@
+import React, { PropTypes } from 'react';
+import Snackbar from 'material-ui/Snackbar';
+/*
+This Component relies on a piece of state passed down in props.
+Should contain, an error, success, & fetching boolean.
+*/
+export default class muiToast extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      show: false,
+      message: '',
+      error: null,
+      fetching: null,
+    };
+  }
+
+  componentWillMount() {
+    this.setState({
+      error: this.props.apiStatus.error,
+      fetching: this.props.apiStatus.fetching,
+    });
+  }
+
+  componentWillReceiveProps(nextProps) {
+    const error = this.state.error;
+    const fetching = this.state.fetching;
+    const apiError = nextProps.apiStatus.error;
+    const apiFetching = nextProps.apiStatus.fetching;
+
+    if (!error && fetching && !apiError && !apiFetching) {
+      // If fetching was successfully completed
+      this.setState({
+        message: "Logged in SUCCESSFULLY!",
+        error: false,
+        fetching: false,
+        show: true,
+      });
+      return true;
+    } else if (!error && apiError) {
+      // if fetching yielded an error
+      this.setState({
+        message: "Database update FAILED!",
+        error: true,
+        fetching: false,
+        show: true,
+      });
+      return true;
+    }
+    return true;
+  }
+
+  render() {
+    const PROPS = {
+      open: this.state.show,
+      message: this.state.message,
+      autoHideDuration: 2000,
+      onRequestClose: () => this.setState({ show: false }),
+    };
+    return (
+      <div>
+        <Snackbar {...PROPS} />
+      </div>
+    );
+  }
+}
+
+muiToast.propTypes = {
+  apiStatus: PropTypes.object, // eslint-disable-line
+};
